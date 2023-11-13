eafef8afd0c0101ea5d767c05efc11c8648410c4
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-11-13 10:03:33 +0100

    test: initial size test before actual implementation

--- src/size.ts
+++ src/size.ts
@@ -1,7 +1,8 @@
+//TODO in installed bip39 to dev-dependencies. Make use i don't need it here
 import type { OutputInstance } from '@bitcoinerlab/descriptors';
 export function size(
-  inputs: Array<{ output: OutputInstance; value: number }>,
-  outputs: Array<{ output: OutputInstance; value: number }>
+  inputs: Array<OutputInstance>,
+  outputs: Array<OutputInstance>
 ) {
   console.log(inputs, outputs);
   return 1;
--- test/size.test.ts
+++ test/size.test.ts
@@ -1,5 +1,68 @@
+import { RegtestUtils } from 'regtest-client';
+import { networks } from 'bitcoinjs-lib';
+import * as secp256k1 from '@bitcoinerlab/secp256k1';
+import { mnemonicToSeedSync } from 'bip39';
+import {
+  DescriptorsFactory,
+  OutputInstance,
+  scriptExpressions
+} from '@bitcoinerlab/descriptors';
+const { Output, BIP32 } = DescriptorsFactory(secp256k1);
+const regtestUtils = new RegtestUtils();
 import { size } from '../dist';
 
-test('Size', () => {
-  expect(size([], [])).toBe(1);
+const MNEMONIC =
+  'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
+const network = networks.regtest;
+
+describe('Size', () => {
+  const inputs: Array<OutputInstance> = [];
+  const outputs: Array<OutputInstance> = [];
+  const masterNode = BIP32.fromSeed(mnemonicToSeedSync(MNEMONIC), network);
+  test('Fund', async () => {
+    const ATTEMPTS = 10;
+    for (let i = 0; i < ATTEMPTS; i++) {
+      try {
+        await regtestUtils.height();
+        break;
+      } catch (err: unknown) {
+        const message = (err as Error).message;
+        console.warn(`Attempt #${i + 1} to access a node: ${message}`);
+        // Wait for 1 sec except after the final attempt
+        if (i < ATTEMPTS - 1)
+          await new Promise(resolve => setTimeout(resolve, 1000));
+      }
+    }
+    await regtestUtils.mine(100);
+
+    const VALUE = 10000;
+    inputs.push(
+      new Output({
+        descriptor: scriptExpressions.wpkhBIP32({
+          masterNode,
+          network,
+          account: 0,
+          keyPath: '/0/0'
+        }),
+        network
+      })
+    );
+    if (!inputs[0]) throw new Error();
+    const unspent = await regtestUtils.faucet(inputs[0].getAddress(), VALUE);
+    expect(unspent.value).toEqual(VALUE);
+  });
+  test('Size', () => {
+    outputs.push(
+      new Output({
+        descriptor: scriptExpressions.wpkhBIP32({
+          masterNode,
+          network,
+          account: 0,
+          keyPath: '/0/1'
+        }),
+        network
+      })
+    );
+    expect(size(inputs, outputs)).toBe(1);
+  });
 });
