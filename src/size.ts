52ff93e0f256f3dcd03377462a0e641cf8ad685c
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-11-13 19:01:38 +0100

    tx size functional, automated tests remaining

--- src/size.ts
+++ src/size.ts
@@ -1,9 +1,143 @@
-//TODO in installed bip39 to dev-dependencies. Make use i don't need it here
+//TODO: npm push new version of descriptors with getScriptSatisfactionSize
+
+// byteLength: https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/ts_src/transaction.ts
+// https://gist.github.com/junderw/b43af3253ea5865ed52cb51c200ac19c
+// https://bitcoinops.org/en/tools/calc-size/
+// https://github.com/bitcoinjs/coinselect/blob/master/utils.js
+
 import type { OutputInstance } from '@bitcoinerlab/descriptors';
+
+function checkUInt53(n: number) {
+  if (n < 0 || n > Number.MAX_SAFE_INTEGER || n % 1 !== 0)
+    throw new RangeError('value out of range');
+}
+
+function varIntLength(number: number) {
+  checkUInt53(number);
+
+  return number < 0xfd
+    ? 1
+    : number <= 0xffff
+    ? 3
+    : number <= 0xffffffff
+    ? 5
+    : 9;
+}
+
+function inputBytes(input: OutputInstance) {
+  const errorMsg =
+    'Input type not implemented. Currently supported: pkh(KEY), wpkh(KEY), sh(wpkh(KEY)), sh(wsh(MINISCRIPT)), sh(MINISCRIPT), wsh(MINISCRIPT)';
+
+  const expandedExpression = input.expand().expandedExpression;
+  if (!expandedExpression) throw new Error('Invalid input');
+
+  if (expandedExpression.startsWith('pkh(')) {
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (script_len:1) + (sig:73) + (pubkey:34)
+    return 148 * 4;
+  } else if (expandedExpression.startsWith('wpkh(')) {
+    // Segwit: (push_count:1) + (sig:73) + (pubkey:34)
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (script_len:1)
+    return 108 + 41 * 4;
+  } else if (expandedExpression.startsWith('sh(wpkh(')) {
+    // Segwit: (push_count:1) + (sig:73) + (pubkey:34)
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (script_len:1) + (p2wpkh:23)
+    //  -> p2wpkh_script: OP_0 OP_PUSH20 <public_key_hash>
+    //  -> p2wpkh: (script_len:1) + (script:22)
+    return 108 + 64 * 4;
+  } else if (expandedExpression.startsWith('sh(wsh(')) {
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (script_len:1) + (p2wsh:35)
+    //  -> p2wsh: uses sha256 instead of hash160, so 12 more bytes than that p2wpkh above
+    //  -> p2wsh_script: OP_0 OP_PUSH32 <sha256_of_script>
+    //  -> p2wsh: (script_len:1) + (script:34)
+    const scriptWitnessSize = input.getScriptSatisfactionSize();
+    if (scriptWitnessSize === undefined) throw new Error(errorMsg);
+    const witnessScriptSize = input.getWitnessScript()?.length;
+    if (witnessScriptSize === undefined) throw new Error(errorMsg);
+    return (
+      varIntLength(scriptWitnessSize) +
+      scriptWitnessSize +
+      varIntLength(witnessScriptSize) +
+      witnessScriptSize +
+      76 * 4
+    );
+  } else if (expandedExpression.startsWith('sh(')) {
+    // Regular sh(MINISCRIPT)
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (locking_len) + (locking) + (unlocking_len) + (ulocking):
+    const scriptSatisfactionSize = input.getScriptSatisfactionSize();
+    if (scriptSatisfactionSize === undefined) throw new Error(errorMsg);
+    const lockingScriptSize = input.getRedeemScript()?.length;
+    if (lockingScriptSize === undefined) throw new Error(errorMsg);
+    return (
+      (varIntLength(scriptSatisfactionSize) +
+        scriptSatisfactionSize +
+        varIntLength(lockingScriptSize) +
+        lockingScriptSize +
+        40) *
+      4
+    );
+  } else if (expandedExpression.startsWith('wsh(')) {
+    // Non-segwit: (txid:32) + (vout:4) + (sequence:4) + (script_len:1)
+    const scriptWitnessSize = input.getScriptSatisfactionSize();
+    if (scriptWitnessSize === undefined) throw new Error(errorMsg);
+    const witnessScriptSize = input.getWitnessScript()?.length;
+    if (witnessScriptSize === undefined) throw new Error(errorMsg);
+    return (
+      varIntLength(scriptWitnessSize) +
+      scriptWitnessSize +
+      varIntLength(witnessScriptSize) +
+      witnessScriptSize +
+      41 * 4
+    );
+  } else {
+    throw new Error(errorMsg);
+  }
+}
+
+function outputBytes(output: OutputInstance) {
+  const expandedExpression = output.expand().expandedExpression;
+  if (!expandedExpression) throw new Error('Invalid output');
+  const errorMsg =
+    'Output type not implemented. Currently supported: pkh(KEY), wpkh(KEY), sh(ANYTHING), wsh(ANYTHING)';
+  if (expandedExpression.startsWith('pkh(')) {
+    // (p2pkh:26) + (amount:8)
+    return 34 * 4;
+  } else if (expandedExpression.startsWith('wpkh(')) {
+    // (p2wpkh:23) + (amount:8)
+    return 31 * 4;
+  } else if (expandedExpression.startsWith('sh(')) {
+    // (p2sh:24) + (amount:8)
+    return 32 * 4;
+  } else if (expandedExpression.startsWith('wsh(')) {
+    // (p2wsh:35) + (amount:8)
+    return 43 * 4;
+  } else {
+    throw new Error(errorMsg);
+  }
+}
+
 export function size(
   inputs: Array<OutputInstance>,
   outputs: Array<OutputInstance>
 ) {
-  console.log(inputs, outputs);
-  return 1;
+  let hasWitness = false;
+  let totalWeight = 0;
+  inputs.forEach(function (input) {
+    totalWeight += inputBytes(input);
+    if (input.isSegwit()) hasWitness = true;
+  });
+  outputs.forEach(function (output) {
+    totalWeight += outputBytes(output);
+  });
+
+  console.log({ hasWitness });
+
+  if (hasWitness) totalWeight += 2;
+
+  totalWeight += 8 * 4;
+  totalWeight += varIntLength(inputs.length) * 4;
+  totalWeight += varIntLength(outputs.length) * 4;
+
+  console.log('COMPUTED TEORICAL MAX SIZE', totalWeight / 4);
+
+  return Math.ceil(totalWeight / 4);
 }
--- test/size.test.ts
+++ test/size.test.ts
@@ -1,13 +1,17 @@
 import { RegtestUtils } from 'regtest-client';
-import { networks } from 'bitcoinjs-lib';
+import { Psbt, networks } from 'bitcoinjs-lib';
 import * as secp256k1 from '@bitcoinerlab/secp256k1';
 import { mnemonicToSeedSync } from 'bip39';
+const { encode: olderEncode } = require('bip68');
+import { compilePolicy } from '@bitcoinerlab/miniscript';
+
 import {
   DescriptorsFactory,
   OutputInstance,
-  scriptExpressions
+  scriptExpressions,
+  signers
 } from '@bitcoinerlab/descriptors';
-const { Output, BIP32 } = DescriptorsFactory(secp256k1);
+const { Output, BIP32, ECPair } = DescriptorsFactory(secp256k1);
 const regtestUtils = new RegtestUtils();
 import { size } from '../dist';
 
@@ -15,9 +19,21 @@ const MNEMONIC =
   'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
 const network = networks.regtest;
 
+const INPUT_VALUE = 10000;
+const FEE_PER_OUTPUT = 10;
+const LOCK = 1;
+
+const POLICY = (older: number) =>
+  `or(pk(@panicKey),99@and(pk(@unvaultKey),older(${older})))`;
+
+const panicPair = ECPair.makeRandom();
+const panicPubKey = panicPair.publicKey;
+const unvaultPair = ECPair.makeRandom();
+const unvaultPubKey = unvaultPair.publicKey;
 describe('Size', () => {
   const inputs: Array<OutputInstance> = [];
   const outputs: Array<OutputInstance> = [];
+  const inputOrigins: Array<{ txHex: string; vout: number }> = [];
   const masterNode = BIP32.fromSeed(mnemonicToSeedSync(MNEMONIC), network);
   test('Fund', async () => {
     const ATTEMPTS = 10;
@@ -35,23 +51,52 @@ describe('Size', () => {
     }
     await regtestUtils.mine(100);
 
-    const VALUE = 10000;
-    inputs.push(
-      new Output({
-        descriptor: scriptExpressions.wpkhBIP32({
-          masterNode,
-          network,
-          account: 0,
-          keyPath: '/0/0'
-        }),
-        network
-      })
-    );
-    if (!inputs[0]) throw new Error();
-    const unspent = await regtestUtils.faucet(inputs[0].getAddress(), VALUE);
-    expect(unspent.value).toEqual(VALUE);
+    let input = new Output({
+      descriptor: scriptExpressions.wpkhBIP32({
+        masterNode,
+        network,
+        account: 0,
+        keyPath: '/0/0'
+      }),
+      network
+    });
+
+    inputs.push(input);
+
+    let unspent = await regtestUtils.faucet(input.getAddress(), INPUT_VALUE);
+    let { txHex } = await regtestUtils.fetch(unspent.txId);
+    inputOrigins.push({ txHex, vout: unspent.vout });
+    expect(unspent.value).toEqual(INPUT_VALUE);
+
+    //Discard pkh
+    inputs.shift();
+    inputOrigins.shift();
+    console.log({ inputs });
+
+    const older = olderEncode({ blocks: LOCK });
+    const { miniscript } = compilePolicy(POLICY(older));
+
+    const triggerDescriptor = `sh(wsh(${miniscript
+      .replace('@unvaultKey', unvaultPubKey.toString('hex'))
+      .replace('@panicKey', panicPubKey.toString('hex'))}))`;
+
+    input = new Output({
+      descriptor: triggerDescriptor,
+      allowMiniscriptInP2SH: true,
+      network,
+      signersPubKeys: [panicPubKey]
+    });
+    inputs.push(input);
+
+    unspent = await regtestUtils.faucet(input.getAddress(), INPUT_VALUE);
+    ({ txHex } = await regtestUtils.fetch(unspent.txId));
+    inputOrigins.push({ txHex, vout: unspent.vout });
+    expect(unspent.value).toEqual(INPUT_VALUE);
   });
+
   test('Size', () => {
+    const psbt = new Psbt({ network });
+
     outputs.push(
       new Output({
         descriptor: scriptExpressions.wpkhBIP32({
@@ -63,6 +108,28 @@ describe('Size', () => {
         network
       })
     );
-    expect(size(inputs, outputs)).toBe(1);
+
+    const finalizers = [];
+    for (const [i, input] of inputs.entries()) {
+      const inputOrigin = inputOrigins[i];
+      if (!inputOrigin) throw new Error('Invalid inputOrigins');
+      finalizers.push(input.updatePsbtAsInput({ psbt, ...inputOrigin }));
+    }
+    outputs.forEach(output => {
+      output.updatePsbtAsOutput({
+        psbt,
+        value: (inputs.length * INPUT_VALUE) / outputs.length - FEE_PER_OUTPUT
+      });
+    });
+    //Discard pkh
+    //signers.signBIP32({ psbt, masterNode });
+    signers.signECPair({ psbt, ecpair: panicPair });
+    finalizers.forEach(finalizer => finalizer({ psbt }));
+
+    //I always count 126 (assuming 2 different data pushes)
+    //Sometimes i get 127
+
+    console.log('SIGNED SIZE', psbt.extractTransaction().virtualSize());
+    expect(psbt.extractTransaction().virtualSize()).toBe(size(inputs, outputs));
   });
 });
