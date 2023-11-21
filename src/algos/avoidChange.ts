f0c8d6aa34c5e8ea2fee785db4322f57d5b02463
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-11-21 11:58:11 +0100

    refactor avoidChange for clarity

--- src/algos/avoidChange.ts
+++ src/algos/avoidChange.ts
@@ -48,12 +48,6 @@ export function avoidChange({
   for (const candidate of utxos) {
     const utxosSoFarValue = utxosSoFar.reduce((a, utxo) => a + utxo.value, 0);
 
-    const txSizeWithCandidate = vsize(
-      [candidate.output, ...utxosSoFar.map(utxo => utxo.output)],
-      targets.map(target => target.output)
-    );
-    const txFeeWithCandidate = Math.ceil(txSizeWithCandidate * feeRate);
-
     const txSizeWithCandidateAndChange = vsize(
       [candidate.output, ...utxosSoFar.map(utxo => utxo.output)],
       [remainder, ...targets.map(target => target.output)]
@@ -69,6 +63,12 @@ export function avoidChange({
 
     //Check that adding the candidate utxo would NOT imply that change was needed:
     if (isDust(remainder, remainderValue, dustRelayFeeRate)) {
+      const txSizeWithCandidate = vsize(
+        [candidate.output, ...utxosSoFar.map(utxo => utxo.output)],
+        targets.map(target => target.output)
+      );
+      const txFeeWithCandidate = Math.ceil(txSizeWithCandidate * feeRate);
+      //Enough utxo value already so that it covers targets and fee?
       if (
         utxosSoFarValue + candidate.value >=
         targetsValue + txFeeWithCandidate
