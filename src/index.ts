17eb7926bf820310ab3b2d630d9c5daed2658d1d
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-11-23 15:37:24 +0100

    reorganize exported defaults

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 // Copyright (c) 2023 Jose-Luis Landabaso - https://bitcoinerlab.com
 // Distributed under the MIT software license
 
+// Same as in Bitcoin Core
+export const DUST_RELAY_FEE_RATE = 3;
+//10 times larger than 2017 peak
+export const MAX_FEE_RATE = 10000;
+
 import type { OutputInstance } from '@bitcoinerlab/descriptors';
-export { vsize } from './vsize';
+export { vsize, inputWeight, outputWeight } from './vsize';
 export { dustThreshold } from './dust';
 export { coinselect } from './coinselect';
 export { maxFunds } from './algos/maxFunds';
 export { addUntilReach } from './algos/addUntilReach';
 export { avoidChange } from './algos/avoidChange';
-
-export const DUST_RELAY_FEE_RATE = 3;
-export const MAX_FEE_RATE = 10 * 1000; //10 times larger than 2017 peak
-export type InputOrigin = { txHex: string; vout: number };
 export type OutputWithValue = { output: OutputInstance; value: number };
